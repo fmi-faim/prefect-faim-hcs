1180cd8c0b6ec9c82f8d7aebae77cdaf71d44c70
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2023-03-09 10:50:08 +0100

    Rename.

--- src/prefect_faim_hcs/flows/molecular_devices_to_ome_zarr_3d.py
+++ src/prefect_faim_hcs/flows/molecular_devices_to_ome_zarr_3d.py
@@ -1,5 +1,6 @@
 from faim_hcs.io.MolecularDevicesImageXpress import parse_files
 from faim_hcs.Zarr import PlateLayout
+from faim_prefect.parallelization.utils import wait_for_task_run
 from prefect import flow
 
 from src.prefect_faim_hcs._version import version
@@ -20,6 +21,8 @@ def build_ome_zarr_plate_3d(
     barcode: str,
     plate_layout: PlateLayout = PlateLayout.I96,
     channels: list[str] = ["w1", "w2", "w3", "w4"],
+    write_empty_chunks: bool = True,
+    parallelization: int = 24,
 ):
     files = parse_files(acquisition_dir=acquisition_dir)
 
@@ -31,12 +34,31 @@ def build_ome_zarr_plate_3d(
         barcode=barcode,
     )
 
-    for well in files["well"].unique():
-        add_well_to_plate_task(
-            zarr_source=plate,
-            files=files,
-            well=well,
-            channels=channels,
+    buffer = []
+    wells = []
+    for well_id in files["well"].unique():
+        buffer.append(
+            add_well_to_plate_task.submit(
+                zarr_source=plate,
+                files=files,
+                well=well_id,
+                channels=channels,
+                write_empty_chunks=write_empty_chunks,
+            )
         )
+
+        wait_for_task_run(
+            results=wells,
+            buffer=buffer,
+            max_buffer_length=parallelization,
+            result_insert_fn=lambda r: r.result(),
+        )
+
+    wait_for_task_run(
+        results=wells,
+        buffer=buffer,
+        max_buffer_length=0,
+        result_insert_fn=lambda r: r.result(),
+    )
 
     return plate
