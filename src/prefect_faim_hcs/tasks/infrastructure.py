b67e85ddeb5f0783e90a2a2bcfc83aa2f6950151
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2023-03-14 15:08:17 +0100

    Add path to micromamba.

--- src/prefect_faim_hcs/tasks/infrastructure.py
+++ src/prefect_faim_hcs/tasks/infrastructure.py
@@ -11,16 +11,14 @@ def log_infrastructure(run_dir: str):
     env = {}
     env.update(os.environ)
 
+    mamba = f"{os.environ['MAMBA_ROOT_PREFIX']}/bin/micromamba"
     env_prefix = os.environ["CONDA_PREFIX"]
-    cmd = (
-        f"micromamba env export -p {env_prefix} >"
-        f" {join(run_dir, 'environment.yaml')}"
-    )
+    cmd = f"{mamba} env export -p {env_prefix} > {join(run_dir, 'environment.yaml')}"
     result = subprocess.run(cmd, shell=True, check=True, env=env)
     result.check_returncode()
 
     cmd = (
-        f"micromamba run -p {env_prefix} pip list --format=freeze >"
+        f"{mamba} run -p {env_prefix} pip list --format=freeze >"
         f" {join(run_dir, 'requirements.txt')}"
     )
     result = subprocess.run(cmd, shell=True, check=True)
